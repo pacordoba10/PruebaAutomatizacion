0c8a34ad93a6ef54309197dd5c0b514c92bcd7f3
alejo-camacho65 <44273917+alejo-camacho65@users.noreply.github.com>
2020-02-21 22:02:46 -0500

    Initial commit

---
+++ PruebaAutomatizacion/Clases/MetodosAutomatizar.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace PruebaAutomatizacion.Clases
+{
+    class MetodosAutomatizar
+    {
+        public void busquedaCargoEmpleos()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.choucairtesting.com/empleos-testing/");
+            IWebElement input = driver.FindElement(By.Id("search_keywords")); // BUSCA ELEMENTO CARGO
+            IWebElement botonbuscar = driver.FindElement(By.ClassName("search_submit")); // BUSCA ELEMENTO BOTON BUSCAR TRABAJOS
+
+            driver.Manage().Window.Maximize();
+
+            input.SendKeys("Analista");
+            botonbuscar.Click();
+            Console.WriteLine("Realizado");
+        }
+
+        public void BusquedaUbicacion()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.choucairtesting.com/empleos-testing/");
+
+            driver.Manage().Window.Maximize();
+            IWebElement input = driver.FindElement(By.Id("search_location"));// BUSCA ELEMENTO UBICACION
+            IWebElement botonbuscar = driver.FindElement(By.ClassName("search_submit")); // BUSCA ELEMENTO BOTON BUSCAR TRABAJOS
+
+            input.SendKeys("Medellin");
+            botonbuscar.Click();
+
+            Console.WriteLine("Realizado");
+
+        }
+
+        public void BusquedaCargoyUbicacion()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.choucairtesting.com/empleos-testing/");
+
+            driver.Manage().Window.Maximize();
+            IWebElement inputl = driver.FindElement(By.Id("search_location"));
+            IWebElement inputk = driver.FindElement(By.Id("search_keywords"));
+            IWebElement botonbuscar = driver.FindElement(By.ClassName("search_submit")); // BUSCA ELEMENTO BOTON BUSCAR TRABAJOS
+
+            inputl.SendKeys("Bogotá");
+            inputk.SendKeys("Psicólogo de selección");
+            botonbuscar.Click();
+            Console.WriteLine("Realizado");
+        }
+
+        public void abrirModeloCalidadSof()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.choucairtesting.com/empleos-testing/");
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine("Cargo pagina");
+
+            IWebElement calidadSoftwate = driver.FindElement(By.LinkText("Modelo de calidad de software"));
+            string a = calidadSoftwate.GetAttribute("href");
+
+            bool b;
+
+            try
+            {
+                
+                driver.Navigate().GoToUrl(a);
+                b = true;
+            }
+            catch (Exception ex)
+            {
+                b = false;
+            }
+
+            string pag = driver.Url;
+            if (pag == a)
+            {
+                Console.WriteLine("Pagina correcta");
+            }
+            else
+            {
+                Console.WriteLine("Pagina error");
+            }
+
+            Console.WriteLine("Cargo Contactenos");
+
+        }
+    }
+}
---
+++ PruebaAutomatizacion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using PruebaAutomatizacion.Clases;
+
+namespace PruebaAutomatizacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetodosAutomatizar mt = new MetodosAutomatizar();
+
+            mt.busquedaCargoEmpleos();
+            mt.BusquedaUbicacion();
+            mt.BusquedaCargoyUbicacion();
+            mt.abrirModeloCalidadSof();
+                        
+
+        }
+    }
+}
